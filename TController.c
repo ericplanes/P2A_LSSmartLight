28f7c2090f530ae3ca408f918d34e1eee42137a5
Eric Planes <#>
2025-06-17 21:24:50 +0200

    First part of modifications CTR

--- TController.c
+++ TController.c
@@ -7,33 +7,44 @@
 #include "TEEPROM.h"
 #include "TUserControl.h"
 #include "THora.h"
+#include "TTimer.h"
 
 /* =======================================
  *              CONSTANTS
  * ======================================= */
 
 #define UID_SIZE 5
-#define UID_STRING_SIZE 15 // "11-22-33-44-55\0"
+#define UID_STRING_SIZE 15
 #define CONFIG_SIZE 6
+
+// States
+#define ST_INIT 0
+#define ST_WAIT_INPUT 1
+#define ST_PROCESS_KEYPAD 2
+#define ST_PROCESS_RFID 3
+#define ST_PROCESS_SERIAL 4
+#define ST_UPDATE_TIME_WAIT 5
+#define ST_UPDATE_STORED_CONFIG 6
 
 /* =======================================
  *         PRIVATE VARIABLES
  * ======================================= */
 
+static BYTE state = ST_INIT;
 static BOOL user_inside;
 static BYTE current_user_uid[UID_SIZE];
 static BYTE current_user_position;
 static BYTE current_config[CONFIG_SIZE];
-static BYTE zero_config[CONFIG_SIZE] = {0, 0, 0, 0, 0, 0};
+static BYTE uid_buffer[UID_STRING_SIZE];
+static BYTE time_hour = 0, time_minute = 0;
 
 /* =======================================
  *       PRIVATE FUNCTION HEADERS
  * ======================================= */
 
-static void format_uid_to_string(BYTE *uid, BYTE *formatted_string);
-static void reset_system_state(void);
-static void copy_uid(BYTE *source, BYTE *destination);
-static BYTE hex_to_char(BYTE hex_value);
+static void format_uid(BYTE *uid);
+static void reset_system(void);
+static BYTE hex_char(BYTE val);
 
 /* =======================================
  *         PUBLIC FUNCTION BODIES
@@ -41,161 +52,161 @@ static BYTE hex_to_char(BYTE hex_value);
 
 void CONTROLLER_Init(void)
 {
-    // Initialize internal state
+    state = ST_INIT;
     user_inside = FALSE;
     current_user_position = USER_NOT_FOUND;
 
-    // Clear UIDs and config
     for (BYTE i = 0; i < UID_SIZE; i++)
         current_user_uid[i] = 0;
     for (BYTE i = 0; i < CONFIG_SIZE; i++)
         current_config[i] = 0;
 
-    // Startup protocol
-    SIO_SendMainMenu();
-    LCD_WriteNoUserInfo();
+    KEY_SetUserInside(FALSE);
 }
 
 void CONTROLLER_Motor(void)
 {
-    BYTE keypad_command;
-    BYTE led_number, led_intensity;
+    BYTE cmd, led, intensity, pos;
     BYTE rfid_uid[UID_SIZE];
-    BYTE formatted_uid[UID_STRING_SIZE];
-    BYTE user_position;
-    BYTE serial_command;
-    BYTE new_hour, new_mins;
 
-    // Step 2: If user is inside, check keypad commands
-    if (user_inside)
+    switch (state)
     {
-        keypad_command = KEY_GetCommand();
+    case ST_INIT:
+        SIO_SendMainMenu();
+        LCD_WriteNoUserInfo();
+        LED_UpdateConfig(current_config); // All zeroes
+        state = ST_WAIT_INPUT;
+        break;
 
-        if (keypad_command == UPDATE_LED)
+    case ST_WAIT_INPUT:
+        if (user_inside)
+            state = ST_PROCESS_KEYPAD;
+        else
+            state = ST_PROCESS_RFID;
+        break;
+
+    case ST_PROCESS_KEYPAD:
+        cmd = KEY_GetCommand();
+        if (cmd == UPDATE_LED)
         {
-            KEY_GetUpdateInfo(&led_number, &led_intensity);
+            KEY_GetUpdateInfo(&led, &intensity);
+            current_config[led] = intensity;
+            KEY_ResetCommand();
+            state = ST_UPDATE_STORED_CONFIG;
+        }
+        else if (cmd == KEYPAD_RESET)
+        {
+            reset_system();
+            KEY_ResetCommand();
+            state = ST_PROCESS_RFID;
+        }
 
-            // Update current config
-            current_config[led_number] = led_intensity;
+        break;
+    case ST_UPDATE_STORED_CONFIG:
+        if (EEPROM_StoreConfigForUser(current_user_position, current_config))
+        {
+            LED_UpdateConfig(current_config);
+            LCD_WriteUserInfo(current_user_uid[4], current_config);
+            state = ST_PROCESS_RFID;
+        }
+        break;
+    case ST_PROCESS_RFID:
+        if (RFID_HasReadUser() && RFID_GetReadUserId(rfid_uid))
+        {
+            pos = USER_FindPositionByRFID(rfid_uid);
 
-            // Store in EEPROM
-            if (EEPROM_StoreConfigForUser(current_user_position, current_config))
+            if (pos == USER_NOT_FOUND)
             {
-                LED_UpdateConfig(current_config);
-                LCD_WriteUserInfo(current_user_uid[UID_SIZE - 1], current_config);
+                format_uid(rfid_uid);
+                SIO_SendUnknownCard(uid_buffer);
             }
-        }
-        else if (keypad_command == KEYPAD_RESET)
-        {
-            reset_system_state();
-            return; // Start fresh next cycle
-        }
-        // If no valid keypad command, continue to step 3
-    }
-
-    // Step 3: Check RFID (if no user inside OR no keypad command processed)
-    if (RFID_HasReadUser())
-    {
-        if (RFID_GetReadUserId(rfid_uid))
-        {
-            user_position = USER_FindPositionByRFID(rfid_uid);
-
-            if (user_position == USER_NOT_FOUND)
+            else if (user_inside && pos == current_user_position)
             {
-                // Unknown card
-                format_uid_to_string(rfid_uid, formatted_uid);
-                SIO_SendUnknownCard(formatted_uid);
+                // User leaving
+                user_inside = FALSE;
+                current_user_position = USER_NOT_FOUND;
+                KEY_SetUserInside(FALSE);
+
+                format_uid(current_user_uid);
+                SIO_SendDetectedCard(uid_buffer, current_config);
+                LCD_WriteNoUserInfo();
+
+                for (BYTE i = 0; i < CONFIG_SIZE; i++)
+                    current_config[i] = 0;
+                LED_UpdateConfig(current_config);
             }
             else
             {
-                // Known user
-                if (user_inside && user_position == current_user_position)
-                {
-                    // Same user leaving
-                    user_inside = FALSE;
-                    current_user_position = USER_NOT_FOUND;
+                // New user entering
+                user_inside = TRUE;
+                current_user_position = pos;
+                for (BYTE i = 0; i < UID_SIZE; i++)
+                    current_user_uid[i] = rfid_uid[i];
 
-                    format_uid_to_string(current_user_uid, formatted_uid);
-                    SIO_SendDetectedCard(formatted_uid, current_config);
-                    LCD_WriteNoUserInfo();
-                    LED_UpdateConfig(zero_config);
+                EEPROM_ReadConfigForUser(pos, current_config);
+                LED_UpdateConfig(current_config);
 
-                    // Clear current user data
-                    for (BYTE i = 0; i < UID_SIZE; i++)
-                        current_user_uid[i] = 0;
-                    for (BYTE i = 0; i < CONFIG_SIZE; i++)
-                        current_config[i] = 0;
-                }
-                else
-                {
-                    // New user entering (or different user)
-                    user_inside = TRUE;
-                    current_user_position = user_position;
-                    copy_uid(rfid_uid, current_user_uid);
-
-                    // Read stored configuration
-                    EEPROM_ReadConfigForUser(user_position, current_config);
-
-                    // Update system
-                    LED_UpdateConfig(current_config);
-                    format_uid_to_string(current_user_uid, formatted_uid);
-                    SIO_SendDetectedCard(formatted_uid, current_config);
-                    LCD_WriteUserInfo(current_user_uid[UID_SIZE - 1], current_config);
-                }
+                format_uid(current_user_uid);
+                SIO_SendDetectedCard(uid_buffer, current_config);
+                LCD_WriteUserInfo(current_user_uid[4], current_config);
+                KEY_SetUserInside(TRUE);
             }
         }
-    }
 
-    // Step 4: Check serial commands
-    serial_command = SIO_ReadCommand();
-
-    switch (serial_command)
-    {
-    case CMD_WHO_IN_ROOM:
-        if (user_inside)
-        {
-            format_uid_to_string(current_user_uid, formatted_uid);
-            SIO_SendUser(formatted_uid);
-        }
-        else
-        {
-            SIO_SendNoUser();
-        }
+        state = ST_PROCESS_SERIAL;
         break;
 
-    case CMD_SHOW_STORED_CONF:
-        for (BYTE pos = 0; pos < NUM_USERS; pos++)
+    case ST_PROCESS_SERIAL:
+        switch (SIO_ReadCommand())
         {
-            const BYTE *stored_uid = USER_GetUserByPosition(pos);
-            BYTE stored_config[CONFIG_SIZE];
-
-            if (stored_uid != (const BYTE *)0)
+        case CMD_WHO_IN_ROOM:
+            if (user_inside)
             {
-                EEPROM_ReadConfigForUser(pos, stored_config);
-                format_uid_to_string((BYTE *)stored_uid, formatted_uid);
-                SIO_SendStoredConfig(formatted_uid, stored_config);
+                format_uid(current_user_uid);
+                SIO_SendUser(uid_buffer);
             }
+            else
+            {
+                SIO_SendNoUser();
+            }
+            break;
+
+        case CMD_SHOW_STORED_CONF:
+            for (pos = 0; pos < NUM_USERS; pos++)
+            {
+                const BYTE *stored_uid = USER_GetUserByPosition(pos);
+                if (stored_uid)
+                {
+                    EEPROM_ReadConfigForUser(pos, current_config);
+                    format_uid((BYTE *)stored_uid);
+                    SIO_SendStoredConfig(uid_buffer, current_config);
+                }
+            }
+            break;
+
+        case CMD_UPDATE_TIME:
+            SIO_SendTimePrompt();
+            state = ST_UPDATE_TIME_WAIT;
+            return;
+
+        case CMD_ESC:
+            SIO_SendMainMenu();
+            break;
+
+        default:
+            break;
         }
+
+        state = ST_WAIT_INPUT;
         break;
 
-    case CMD_UPDATE_TIME:
-        SIO_SendTimePrompt();
-        // Wait until time is read
-        while (!SIO_ReadTime(&new_hour, &new_mins))
+    case ST_UPDATE_TIME_WAIT:
+        if (SIO_ReadTime(&time_hour, &time_minute))
         {
-            // Keep checking (cooperative)
-            return; // Will continue next cycle
+            HORA_SetTime(time_hour, time_minute);
+            LCD_UpdateTime(time_hour, time_minute);
+            state = ST_WAIT_INPUT;
         }
-        HORA_SetTime(new_hour, new_mins);
-        break;
-
-    case CMD_ESC:
-        SIO_SendMainMenu();
-        break;
-
-    case CMD_NO_COMMAND:
-    default:
-        // No action needed
         break;
     }
 }
@@ -204,55 +215,36 @@ void CONTROLLER_Motor(void)
  *        PRIVATE FUNCTION BODIES
  * ======================================= */
 
-static void format_uid_to_string(BYTE *uid, BYTE *formatted_string)
+static void format_uid(BYTE *uid)
 {
     BYTE pos = 0;
-
     for (BYTE i = 0; i < UID_SIZE; i++)
     {
-        // High nibble
-        formatted_string[pos++] = hex_to_char((uid[i] >> 4) & 0x0F);
-        // Low nibble
-        formatted_string[pos++] = hex_to_char(uid[i] & 0x0F);
-
-        // Add hyphen except after last byte
+        uid_buffer[pos++] = hex_char((uid[i] >> 4) & 0x0F);
+        uid_buffer[pos++] = hex_char(uid[i] & 0x0F);
         if (i < UID_SIZE - 1)
-            formatted_string[pos++] = '-';
+            uid_buffer[pos++] = '-';
     }
-
-    formatted_string[pos] = '\0'; // Null terminate
+    uid_buffer[pos] = '\0';
 }
 
-static void reset_system_state(void)
+static void reset_system(void)
 {
-    // Clear all memory
     EEPROM_CleanMemory();
-
-    // Reset system state
     user_inside = FALSE;
     current_user_position = USER_NOT_FOUND;
 
-    // Clear data
     for (BYTE i = 0; i < UID_SIZE; i++)
         current_user_uid[i] = 0;
     for (BYTE i = 0; i < CONFIG_SIZE; i++)
         current_config[i] = 0;
 
-    // Update hardware
-    LED_UpdateConfig(zero_config);
+    LED_UpdateConfig(current_config);
     LCD_WriteNoUserInfo();
+    KEY_SetUserInside(FALSE);
 }
 
-static void copy_uid(BYTE *source, BYTE *destination)
+static BYTE hex_char(BYTE val)
 {
-    for (BYTE i = 0; i < UID_SIZE; i++)
-        destination[i] = source[i];
-}
-
-static BYTE hex_to_char(BYTE hex_value)
-{
-    if (hex_value < 10)
-        return '0' + hex_value;
-    else
-        return 'A' + (hex_value - 10);
+    return (val < 10) ? ('0' + val) : ('A' + val - 10);
 }
